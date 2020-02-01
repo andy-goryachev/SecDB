7a8e1b198421e257050c174e82b98d648e46fa38
andy goryachev <andy@goryachev.com>
2020-02-01 15:16:23 -0800

    crypto

---
+++ src/goryachev/crypto/OpaqueChars.java
@@ -0,0 +1,232 @@
+// Copyright © 2011-2020 Andy Goryachev <andy@goryachev.com>
+package goryachev.crypto;
+import goryachev.common.util.CKit;
+
+
+/**
+ * This class provides an opaque storage for a char array.
+ */
+public final class OpaqueChars
+	extends OpaqueMemObject
+{
+	public OpaqueChars(char[] cs)
+	{
+		set(cs);
+	}
+	
+	
+	public OpaqueChars(OpaqueChars op)
+	{
+		super(op);
+	}
+	
+	
+	public OpaqueChars(byte[] b)
+	{
+		super(b);
+	}
+	
+	
+	public OpaqueChars()
+	{
+	}
+	
+	
+	public void set(OpaqueChars x)
+	{
+		char[] cs = null;
+		try
+		{
+			if(x != null)
+			{
+				cs = x.getChars();
+			}
+			
+			set(cs);
+		}
+		finally
+		{
+			Crypto.zero(cs);
+		}
+	}
+	
+	
+	public void set(String s)
+	{
+		char[] cs = null;
+		try
+		{
+			if(s != null)
+			{
+				cs = s.toCharArray();
+			}
+			
+			set(cs);
+		}
+		finally
+		{
+			Crypto.zero(cs);
+		}
+	}
+	
+	
+	public final void set(char[] cs)
+	{
+		byte[] b = null;
+		try
+		{
+			if(cs != null)
+			{
+				int sz = cs.length;
+				b = new byte[sz + sz];
+				int ix = 0;
+				for(int i=0; i<sz; i++)
+				{
+					int c = cs[i];
+					b[ix++] = (byte)(c >>> 8);
+					b[ix++] = (byte)c;
+				}
+			}
+			
+			setBytes(b);
+		}
+		finally
+		{
+			Crypto.zero(b);
+		}
+	}
+	
+	
+	public final char[] getChars()
+	{
+		byte[] b = null;
+		try
+		{
+			b = getBytes();
+			int sz = b.length/2;
+			
+			char[] cs = new char[sz];
+			int ix = 0;
+			for(int i=0; i<sz; i++)
+			{
+				int c = (b[ix++] & 0xff) << 8;
+				c |= (b[ix++] & 0xff);
+				cs[i] = (char)c;
+			}
+			
+			return cs;
+		}
+		finally
+		{
+			Crypto.zero(b);
+		}
+	}
+	
+	
+	public boolean sameAs(OpaqueChars cs)
+	{
+		if(cs == null)
+		{
+			return false;
+		}
+		
+		byte[] me = getBytes();
+		try
+		{
+			byte[] him = cs.getBytes();
+			try
+			{
+				return CKit.equals(me, him);
+			}
+			finally
+			{
+				Crypto.zero(him);
+			}
+		}
+		finally
+		{
+			Crypto.zero(me);
+		}
+	}
+
+
+	public void append(String s)
+	{
+		if(s != null)
+		{
+			append(s.toCharArray());
+		}
+	}
+	
+	
+	public void append(char[] add)
+	{
+		char[] cs = getChars();
+		try
+		{
+			char[] rv = new char[cs.length + add.length];
+			try
+			{
+				System.arraycopy(cs, 0, rv, 0, cs.length);
+				System.arraycopy(add, 0, rv, cs.length, add.length);
+				set(rv);
+			}
+			finally
+			{
+				Crypto.zero(rv);
+			}
+		}
+		finally
+		{
+			Crypto.zero(cs);
+		}
+	}
+
+
+	public void deleteLastChar()
+	{
+		char[] cs = getChars();
+		try
+		{
+			int len = cs.length - 1;
+			if(len >= 0)
+			{
+				// TODO this does not handle surrogate characters
+				char[] rv = new char[len];
+				try
+				{
+					System.arraycopy(cs, 0, rv, 0, len);
+					set(rv);
+				}
+				finally
+				{
+					Crypto.zero(rv);
+				}
+			}
+			
+		}
+		finally
+		{
+			Crypto.zero(cs);
+		}
+	}
+	
+	
+	public OpaqueChars copy()
+	{
+		return new OpaqueChars(this);
+	}
+	
+	
+	public static OpaqueChars copy(OpaqueChars x)
+	{
+		if(x == null)
+		{
+			return null;
+		}
+		else
+		{
+			return x.copy();
+		}
+	}
+}
---
+++ src/goryachev/crypto/OpaqueMemObject.java
@@ -0,0 +1,118 @@
+// Copyright © 2011-2020 Andy Goryachev <andy@goryachev.com>
+package goryachev.crypto;
+
+/**
+ * This class provides additional level of security by storing only the encrypted value in memory.
+ * To avoid also keeping the key in memory, the key is generated each time from information
+ * that stays constant during JVM session per hashCode() contract.
+ */
+abstract class OpaqueMemObject
+{
+	private byte[] encrypted;
+	
+	
+	protected OpaqueMemObject()
+	{
+	}
+	
+	
+	protected OpaqueMemObject(OpaqueMemObject x)
+	{
+		encrypted = x.encrypted;
+	}
+	
+	
+	protected OpaqueMemObject(byte[] b)
+	{
+		setBytes(b);
+	}
+	
+	
+	public final boolean isNull()
+	{
+		return (encrypted == null);
+	}
+	
+	
+	protected final void setBytes(byte[] value)
+	{
+		if(value == null)
+		{
+			encrypted = null;
+		}
+		else
+		{
+			try
+			{
+				encrypted = MemCrypt.encrypt(value);
+			}
+			catch(Exception e)
+			{
+				// should not happen
+				throw new Error(e);
+			}
+		}
+	}
+	
+	
+	protected final void setBytes(byte[] value, int off, int len)
+	{
+		byte[] v = new byte[len];
+		try
+		{
+			System.arraycopy(value, off, v, 0, len);
+			
+			try
+			{
+				encrypted = MemCrypt.encrypt(value);
+			}
+			catch(Exception e)
+			{
+				// should not happen
+				throw new Error(e);
+			}
+		}
+		finally
+		{
+			Crypto.zero(v);
+		}
+	}
+	
+	
+	public final String toString()
+	{
+		return String.valueOf('*');
+	}
+	
+	
+	/**
+	 * Returns decrypted byte array representing the stored object.
+	 */
+	public final byte[] getBytes()
+	{
+		if(encrypted == null)
+		{
+			return null;
+		}
+		else
+		{
+			try
+			{
+				byte[] b = MemCrypt.decrypt(encrypted);
+				return b;
+			}
+			catch(Exception e)
+			{
+				// should not happen
+				throw new Error(e);
+			}
+		}
+	}
+	
+	
+	public final void clear()
+	{
+		Crypto.zero(encrypted);
+		encrypted = null;
+	}
+}
